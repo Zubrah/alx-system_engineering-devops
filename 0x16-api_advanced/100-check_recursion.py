f1a8489a8c21f14d22f0a710dd10cb9d57b55437
Zubeir Msemo <44870879+Zubrah@users.noreply.github.com>
2023-08-11 15:29:39 +0300

    Update 100-check_recursion.py

--- 0x16-api_advanced/100-check_recursion.py
+++ 0x16-api_advanced/100-check_recursion.py
@@ -1,31 +1,56 @@
 #!/usr/bin/python3
-'''
-Module 2-recurse
-retrieve list containing titlesof hot articles for a given subreddit
-'''
-import requests
+"""
+Traceback function to verify that a student's function is recursive.
+"""
+import sys
+import traceback
+
+from contextlib import redirect_stdout
+from io import StringIO
 
 
-def recurse(subreddit, hot_list=[], after='tmp'):
-    '''
-    recursvily get hot artilce titles for subreddit
-    '''
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'linux'}
-    if after != 'tmp':
-        url = url + '?after={}'.format(after)
+def check_recursion():
+    """
+    Verifies that a student's code is using recursion.
+    """
+    sys.settrace(trace_calls)
+    null = StringIO()
+    trace = StringIO()
+    stderr = sys.stderr
+    sys.stderr = trace
 
-    r = requests.get(url, headers=headers, allow_redirects=False)
-    if r.status_code == 404:
-        return None
-    res = r.json().get('data')
-    if not res:
-        return hot_list
-    'apend artils to list'
-    for a in res.get('children'):
-        hot_list.append(a.get('data').get('title'))
-        'get next param'
-    after = res.get('after')
-    if not after:
-        return hot_list
-    return (recurse(subreddit, hot_list, after))
+    with redirect_stdout(null):
+        count_words('hello', ['hello'])  # r/hello is an existing subreddit which almost certainly contains 'hello'
+    sys.stderr = stderr              # in its posts
+
+    if len(trace.getvalue()) > 0:
+        print("function is recursive", end="")
+    else:
+        print("function is not recursive", end="")
+
+
+def trace_calls(frame, event, arg):
+    if event != 'call': # We only want function calls!
+        return
+    co = frame.f_code
+    func_name = co.co_name
+    if func_name != 'count_words': # We only want function calls of THIS function
+        return
+    func_line_no = frame.f_lineno
+    func_filename = co.co_filename
+    caller = frame.f_back
+    caller_line_no = caller.f_lineno
+    caller_filename = caller.f_code.co_filename
+    if 'main' in caller_filename: # Ignore any calls made in the main
+        return
+    sys.stderr.write('Call to {} on line {} of {} from line {} of {}\n'
+                     .format(func_name,
+                             func_line_no,
+                             func_filename,
+                             caller_line_no,
+                             caller_filename))
+
+
+if __name__ == "__main__":
+    count_words = __import__("100-count").count_words
+    check_recursion()
